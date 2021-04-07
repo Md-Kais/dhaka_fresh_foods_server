c3b6b8e8bef85a964859e749264176c227ca125e
Md-Kais <mdkais3@gmail.com>
2021-04-08 00:14:01 +0600

    dunno

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express=require('express')
-const bodyParser=require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
-const ObjectId=require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectID;
 const app = express()
 var admin = require("firebase-admin");
 const MongoClient = require('mongodb').MongoClient;
@@ -15,28 +15,33 @@ app.use(bodyParser.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjixa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var serviceAccount = require("./dhakafreshfoods-firebase-adminsdk-h3leb-86f33fbfc9.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+// var serviceAccount = require("./dhakafreshfoods-firebase-adminsdk-h3leb-86f33fbfc9.json");
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
+const firebaseAdminSdk = require('firebase-admin'),
+    firebaseAdminApp = firebaseAdminSdk.initializeApp({
+        credential: firebaseAdminSdk.credential.cert(
+            JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii')))
+    });
 
 client.connect(err => {
     console.log(uri);
     console.log(err);
     const collection = client.db("dhakaFreshFood").collection("foodItems");
-    app.post('/addProducts',(req,res)=>{
-            console.log(req.body);
-            console.log('done');
-            collection.insertOne(req.body)
-            .then(result=>{
+    app.post('/addProducts', (req, res) => {
+        console.log(req.body);
+        console.log('done');
+        collection.insertOne(req.body)
+            .then(result => {
                 console.log(result);
-               res.send(result.insertedCount > 0);
+                res.send(result.insertedCount > 0);
             })
-        })
-    app.get('/',(req,res)=>{
-            collection.find()
-            .toArray((err,items)=>{
+    })
+    app.get('/', (req, res) => {
+        collection.find()
+            .toArray((err, items) => {
                 res.send(items);
             })
     })
