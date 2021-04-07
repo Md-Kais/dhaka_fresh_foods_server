3104c2f181967019fbb1456eda3c25ee7b300255
Md-Kais <mdkais3@gmail.com>
2021-04-07 22:10:41 +0600

    adding admin panels

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(bodyParser.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjixa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 client.connect(err => {
     console.log(uri);
     console.log(err);
